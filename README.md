9ea5b0c5ab3a8000f434db98f2096c38875f6aff
elgumso <michael@elgumso.com>
2022-02-14 16:23:42 +1100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# fishblood
-Nightscout Blood Glucose Values in your Fish terminal prompt
+# Fishblood
+## Nightscout Blood Glucose Values in your Fish terminal prompt
+
+Having recently been diagnosed with Type 1 Diabetes, I wanted a way to see my Blood Glucose Values at a glance when working from my terminal, and a quick way to to configure this for all the terminals and systems I work with. Fishblood is the quick hacky way I accomplished this.
+
+### Dependencies
+This is tested as working on Ubuntu with `Fish 3.3.1`, and requires the JSON tool `jq`, as well as a working `Nightscout` instance, or any server endpoint where the Blood Glucose Value and Delta values are updated and can be pulled from. 
+
+<i>Note: Uncomment the lines in the installation script to install the latest version of Fish if necessary.</i> 
+
+ - <a href="https://nightscout.github.io/">Nightscout</a>
+ - <a href="https://fishshell.com/">Fish shell</a>
+
+### Usage
+
+Installation:
+```console
+  chmod +x fishblood_install.sh
+  ./fishblood_install.sh
+```
+The installation script will backup the `prompt_login.fish` file to the users home directory. If anything goes wrong just restore this file over the original prompt file in `/usr/share/fish/functions/`
+
+### WSL
+When running WSL the `cron` service is not started by default. The service can be started manually on Ubuntu using the following command: 
+```console
+  sudo service cron start
+```
+<i>Note: this only needs to be done in the first instance of starting WSL.</i>
+
+### Screenshots
+
+![Fishblood](./fishblood.PNG)
---
+++ fishblood_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## Config
+NS_URL="https://blood.elgumso.com"	# Nightscout instance URL
+updateInt="1"	 			        # Update interval in minutes
+bloodFile="/tmp/blood"		        # Location to store updated values
+promptFile="/usr/share/fish/functions/prompt_login.fish"
+xUser=$USER
+
+echo -e "\n[+] Fishblood - elGumso 2022"
+echo "[+] Installation script."
+
+## Install deps
+echo -e "\n[+] Installing dependencies..."
+sudo apt install jq -y
+
+## Install latest version of Fish
+# sudo apt-add-repository ppa:fish-shell/release-3
+# sudo apt update
+# sudo apt install fish
+
+## Setup Crontab to update Bloodsugar values
+echo -e "\n[+] Setting up Crontab for user $xUser"
+(crontab -l; echo "*/$updateInt * * * * curl -s $NS_URL/pebble | jq -r '.bgs | .[] | [.sgv, .direction, .bgdelta] | @tsv' | sed 's|\"||g' > $bloodFile") |awk '!x[$0]++'|crontab -
+
+# Backup Fish prompt file
+echo "[+] Backing up Fish prompt file - $promptFile"
+cp /usr/share/fish/functions/prompt_login.fish ~/.prompt_login.fish
+echo "[+] Fish prompt file backed up at ~/.prompt_login.fish"
+
+## Check if modified prompt already installed
+echo -e "\n[+] Checking if Fishblood has been configured previously..."
+if grep -Fq "cat /tmp/blood" $promptFile
+then
+    echo -e "[+] Fishblood has already been configured in $promptFile"
+else
+    ## Modify Fish Shell prompt
+    echo "[+] Modifying Fish login prompt..."
+    sudo sed -i "s%echo -n -s (set_color \$fish_color_user) \"\$USER\" (set_color normal) @ (set_color \$color_host) (prompt_hostname) (set_color normal)%echo -n -s (set_color \$fish_color_user) \"\$USER\" (set_color normal) @ (set_color \$color_host) (prompt_hostname) (set_color normal) '\ \-\-\> ' \ (set_color \$fish_color_host_remote) (cat /tmp/blood | awk '{print \$1}') (set_color normal) '\ ' \{(cat /tmp/blood | awk '{print \$3}')\}%" \
+    $promptFile
+fi
+
+echo -e "\n[+] Fishblood hopefully configured!\n"
